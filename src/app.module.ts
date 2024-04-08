9058a14e94def32c850088e78434cf5b72f8eb35
lcc <1106751718@qq.com>
2024-04-08 09:18:23 +0800

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/userInfo'),
+    MongooseModule.forRoot('mongodb://47.99.117.111:27017/userInfo'),
     UserModule
 ],
   controllers: [],
